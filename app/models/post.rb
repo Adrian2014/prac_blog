cb5abfe1bb8e3c96ca9a9f62a12e747a0a7a7907
Adrian <modernamun@gmail.com>
2014-10-15 19:01:16 -0400

    round two

--- app/models/post.rb
+++
@@ -1,6 +0,0 @@
-class Post < ActiveRecord::Base
-	validates_presence_of :title, :body
-	validates_uniqueness_of :title
-
-	has_many :comments, :dependent => :destroy
-end
--- app/views/comments/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @comment, :id, :post_id, :name, :body, :created_at, :updated_at
