5e91a12fcc2a65edefc2e1cbf50c5a1db0605ca8
Adrian <modernamun@gmail.com>
2014-10-16 13:34:05 -0400

    added create and deleting of comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ActiveRecord::Base
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :text, presence: true, length: { minimum: 12 }
 
-	has_many :comments
+	has_many :comments, dependent: :destroy
 end
